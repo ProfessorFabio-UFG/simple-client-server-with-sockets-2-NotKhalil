56783e0e568e598e34b598a29b3fb10b0b146f7d
Khalil Motta <khalil.motta@discente.ufg.br>
2025-03-29 20:03:19 -0300

    Modified client and server application to a encryption service

--- client.py
+++ client.py
@@ -3,7 +3,20 @@ from constCS import * #-
 
 s = socket(AF_INET, SOCK_STREAM)
 s.connect((HOST, PORT)) # connect to server (block until accepted)
-s.send(str.encode('Hello, world'))  # send some data
+cypher_num = input("Choose the type of cypher:\n(1)reverse\n(2)caesar\n(3)random\n")
+message = ''
+match cypher_num:
+    case '1':
+        message += "reverse "
+    case '2':
+        message += "caesar "
+    case '3':
+        message += "random "
+
+plain_text = input("Type the text to be encrypted:\n")
+message += r'"' + plain_text + r'"'
+
+s.send(str.encode(message))  # send some data
 data = s.recv(1024)     # receive the response
 print (bytes.decode(data))            # print the result
 s.close()               # close the connection
--- server.py
+++ server.py
@@ -1,6 +1,49 @@
 from socket  import *
 from constCS import * #-
 
+import re
+import random
+
+def reverse_encrypt(plain_text):
+  encrypted_text = ''
+  i = len(plain_text) -1
+
+  while(i >= 0):
+    encrypted_text = encrypted_text + plain_text[i]
+    i = i - 1
+  return encrypted_text
+
+def caesar_encrypt(plain_text):
+  encrypted_text = ''
+
+  for i in range(len(plain_text)):
+    char = plain_text[i]
+    displacement = 4
+
+    if(char.isupper()):
+      encrypted_text += chr((ord(char)- 65 + displacement)%26 + 65)
+    elif(char.islower()):
+      encrypted_text += chr((ord(char)- 97 + displacement)%26 + 97)
+    else:
+      encrypted_text += char
+    
+  return encrypted_text
+
+def random_encrypt(plain_text):
+  encrypted_text = ""
+
+  for i in range(len(plain_text)):
+    char = plain_text[i]
+
+    if(char.isupper()):
+      encrypted_text += chr((ord(char)- 65 + random.randint(0,26))%26 + 65)
+    elif(char.islower()):
+      encrypted_text += chr((ord(char)- 97 + random.randint(0,26))%26 + 97)
+    else:
+      encrypted_text += char
+  
+  return encrypted_text
+
 s = socket(AF_INET, SOCK_STREAM) 
 s.bind((HOST, PORT))  #-
 s.listen(1)           #-
@@ -8,6 +51,22 @@ s.listen(1)           #-
 while True:                # forever
   data = conn.recv(1024)   # receive data from client
   if not data: break       # stop if client stopped
-  print(bytes.decode(data))
-  conn.send(str.encode(bytes.decode(data)+"*")) # return sent data plus an "*"
+  message = data.decode()
+
+  match = re.match(r'(\w+)\s+\"([^"]+)\"', message)
+  cypher_type = match.group(1)
+  plain_text = match.group(2)
+
+  match cypher_type:
+    case 'reverse':
+      encrypted_text = reverse_encrypt(plain_text)
+      conn.send(str.encode(encrypted_text))
+    case 'caesar':
+      encrypted_text = caesar_encrypt(plain_text)
+      conn.send(str.encode(encrypted_text))
+    case 'random':
+      encrypted_text = random_encrypt(plain_text)
+      conn.send(str.encode(encrypted_text))
+
+  
 conn.close()               # close the connection
